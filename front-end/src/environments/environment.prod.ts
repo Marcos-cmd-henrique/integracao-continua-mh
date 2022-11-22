dcb2db363e35f3dba49e84c43d35cabd22727140
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-11-21 19:06:14 -0500

    trocado domínio do back-end

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  API_URL: 'https://sgcm-marcoshenrique-sgcm-marcoshenrique.up.railway.app'
+  API_URL: 'https://sgcm-marcoshenrique-production.up.railway.app'
 };
+
+
+//https://sgcm-marcoshenrique-sgcm-marcoshenrique.up.railway.app
